18306cdddb2a17756fd29e97dd953854a3d116c2
Bojan Banović <113462851+bokab@users.noreply.github.com>
2022-10-20 23:30:15 +0200

    ToDoList

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// da bi dobili pristup pomocu node.js na data.js moramo unijeti donji primer!
+// sa require ce uci u date.js i pokusati pokrenuti nas kod
+const date = require(__dirname + "/node_modules/date.js");
+
+const app = express();
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
+
+app.set('view engine', 'ejs');
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.get('/', function (req, res) {
+
+
+    const day = date.getDate();
+
+
+    res.render("list", { listTitle: day, NewListItems: items });
+
+});
+
+
+
+app.post("/", function (req, res) {
+    // newItem = name 
+    const item = req.body.newItem;
+
+    // list = name
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
+
+
+
+app.get("/work", function (req, res) {
+
+    res.render("list", { listTitle: "Work List", NewListItems: workItems });
+});
+
+
+
+app.post("/work", function (req, res) {
+
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
+// ubacivanjem about.ejs i prebacivanje istog na sajt 
+app.get("/about", function (req, res) {
+    res.render("about");
+})
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 4000, function () {
+    console.log('Server is running on port 4000');
+});
+
+
